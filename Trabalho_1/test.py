c09ed866c04c3c1d4c2ac56c94be1c6956531e5f
eduardomeloc <eduardomelo@dcc.ufrj.br>
2021-04-27 21:50:54 -0300

    added python tester

--- Trabalho_1/test.py
+++ Trabalho_1/test.py
@@ -1,11 +1,16 @@
 import subprocess
+import statistics
 import sys
 
 with open("out.txt", "w+") as fout:
-    times = []
-    for i in range(5):
-        out = subprocess.call(["./main.o", "0", "1"], stdout = fout);
-        fout.seek(0)
-        output = fout.read()
-        times.append(int(output[-6:-2]))
-    print(max(times))
+    for nthreads in range(5):
+        for nsamples in [1, 4, 16, 64]:
+            times = []
+            for i in range(5):
+                out = subprocess.call(["./main.o", str(nthreads), str(nsamples)], stdout = fout);
+                fout.seek(0)
+                output = fout.read()
+                times.append(float(output[output.rindex("\t")+1:-2]))
+                print(times)
+            print("nthreads = ", nthreads, "nsamples = ", nsamples)
+            print(statistics.mean(times), "\n")
